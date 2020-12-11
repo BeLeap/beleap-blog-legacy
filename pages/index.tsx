32514c3fe2ff1e56bf009a13624107e6968b03c4
BeLeap <changseo_jang@korea.ac.kr>
2020-12-11 22:24:16 +0900

    remove unused field

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,13 +20,10 @@ export default function Home({
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <section className={utilStyles.headingMd}>
-                <p>BeLeap Blog</p>
-            </section>
             <section
                 className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
             >
-                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <h2 className={utilStyles.headingLg}>Articles</h2>
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
