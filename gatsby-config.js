f50425881cbd6970743a8c1cd0df07fd9bb61222
Changseo Jang <changseo.jang@riiid.co>
2021-06-24 15:41:41 +0900

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,21 @@ module.exports = {
     },
     'gatsby-plugin-emotion',
     'gatsby-plugin-typescript',
-    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `png`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50
+      }
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet'
   ]
