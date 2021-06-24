cfba3bb9f1d5d6c396bc8e009f4f665cb1389762
Changseo Jang <changseo.jang@riiid.co>
2021-06-24 14:12:23 +0900

    change to rem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const StyledHeader = styled.header`
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
   backdrop-filter: blur(4px);
   -webkit-backdrop-filter: blur(4px);
-  border-radius: 10px;
+  border-radius: 1rem;
   border: 1px solid rgba(255, 255, 255, 0.18);
   position: fixed;
   z-index: 999;
