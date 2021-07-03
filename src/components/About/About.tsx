bf8cc25692602a565adcb5b6344097ef58f0e1ad
BeLeap <changseo_jang@korea.ac.kr>
2021-07-03 15:54:52 +0900

    Add about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,39 @@
+import styled from '@emotion/styled'
 import * as React from 'react'
 
+const AboutWrapper = styled.div`
+  font-size: 1.5rem;
+`
+
+const Introduce = styled.div`
+  font-weight: bold;
+  font-size: 2rem;
+`
+
+const ImportantAnchor = styled.a`
+  font-weight: bold;
+`
+
 const About: React.FC = () => {
-  return <>Hi, I'm BeLeap</>
+  return (
+    <AboutWrapper>
+      <Introduce>
+        Hi, I'm Changseo Jang(a.k.a <a href="https://github.com/BeLeap">BeLeap</a>)
+        <br />I Love Develop Something
+      </Introduce>
+      <h2>Biography</h2>
+      Computer Science & Engineering @ <ImportantAnchor href="http://korea.edu">Korea Univ.</ImportantAnchor>
+      <br />
+      Backend Engineer @ <ImportantAnchor href="https://riiid.co">Riiid</ImportantAnchor>
+      <h2>Tech Spec</h2>
+      Using <ImportantAnchor href="https://spring.io">Spring</ImportantAnchor> &{' '}
+      <ImportantAnchor href="https://grpc.io">gRPC</ImportantAnchor> at <ImportantAnchor href="https://riiid.co">Riiid</ImportantAnchor> as
+      Backend Engineer
+      <br />
+      Using <ImportantAnchor href="https://rust-lang.org">Rust</ImportantAnchor> at{' '}
+      <ImportantAnchor href="https://github.com/OxideEngine">OxideEngine</ImportantAnchor>
+    </AboutWrapper>
+  )
 }
 
 export default About
