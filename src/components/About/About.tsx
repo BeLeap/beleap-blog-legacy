e80250fade522640ff5e29ffc8adeb7bdda48595
BeLeap <changseo_jang@korea.ac.kr>
2021-07-03 16:01:17 +0900

    add kotlin to about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -19,14 +19,15 @@ const About: React.FC = () => {
     <AboutWrapper>
       <Introduce>
         Hi, I'm Changseo Jang(a.k.a <a href="https://github.com/BeLeap">BeLeap</a>)
-        <br />I Love Develop Something
+        <br />I Love to Develop Anything
       </Introduce>
       <h2>Biography</h2>
       Computer Science & Engineering @ <ImportantAnchor href="http://korea.edu">Korea Univ.</ImportantAnchor>
       <br />
       Backend Engineer @ <ImportantAnchor href="https://riiid.co">Riiid</ImportantAnchor>
       <h2>Tech Spec</h2>
-      Using <ImportantAnchor href="https://spring.io">Spring</ImportantAnchor> &{' '}
+      Using <ImportantAnchor href="https://spring.io">Spring</ImportantAnchor> (with{' '}
+      <ImportantAnchor href="https://kotlinlang.org">Kotlin</ImportantAnchor>) &{' '}
       <ImportantAnchor href="https://grpc.io">gRPC</ImportantAnchor> at <ImportantAnchor href="https://riiid.co">Riiid</ImportantAnchor> as
       Backend Engineer
       <br />
