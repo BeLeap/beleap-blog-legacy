4006d40935def4fd752539aa2af8ca52f8bd948d
BeLeap <changseo_jang@korea.ac.kr>
2021-07-03 16:43:58 +0900

    add neumorphism effect to execrpt

--- src/components/Article/ArticleCard.tsx
+++ src/components/Article/ArticleCard.tsx
@@ -14,19 +14,30 @@ const Card = styled.a`
   background: #e0e0e0;
   box-shadow: 0.5rem 0.5rem 1rem #bebebe, -0.5rem -0.5rem 1rem #ffffff;
   padding: 2rem;
+  padding-bottom: 0.5rem;
   margin-bottom: 2rem;
 
   &:hover {
     background: #e0e0e0;
     box-shadow: inset 0.5rem 0.5rem 1rem #bebebe, inset -0.5rem -0.5rem 1rem #ffffff;
+    transition: box-shadow 1s ease-in-out;
   }
+`
+
+const ExcerptWrapper = styled.p`
+  border-radius: 1.5rem;
+  background: #e0e0e0;
+  box-shadow: inset 0.5rem 0.5rem 1rem #bebebe, inset -0.5rem -0.5rem 1rem #ffffff;
+  transition: box-shadow 1s ease-in-out;
+  margin-top: 2rem;
+  padding: 2rem;
 `
 
 const ArticleCard: React.FC<ArticleCardProps> = ({ link, title, excerpt }) => {
   return (
     <Card href={link}>
       <h1>{title}</h1>
-      <p>{excerpt}</p>
+      <ExcerptWrapper>{excerpt}</ExcerptWrapper>
     </Card>
   )
 }
